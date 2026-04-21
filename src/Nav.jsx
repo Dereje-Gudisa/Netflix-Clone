e52fb2cc06421562eec0380c73d4106462487c0f
https://github.com/Dereje-Gudisa <derejegudisaeme@gmail.com>
2026-04-22 01:18:06 +0300

    nav setup

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -7,17 +7,20 @@ function Nav() {
   return (
     <>
         <nav className='nav'>
-            <img className='netflix-home-logo' src={logo} alt="Netflix" />
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Shows</a></li>
-                <li><a href="#">Movies</a></li>
-                <li><a href="#">Games</a></li>
-                <li><a href="#">Latest</a></li>
-                <li><a href="#">My List</a></li>
-                <li><a href="#">Browse by Language</a></li>
-            </ul>
-            <ul>
+            <div className='nav-left-container'>
+                <img className='netflix-home-logo' src={logo} alt="Netflix" />
+                <ul className='nav-links nav-links-left'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Shows</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Latest</a></li>
+                    <li><a href="#">My List</a></li>
+                    <li><a href="#">Browse by Language</a></li>
+                </ul>
+            </div>
+            <ul className='nav-links nav-links-right'>
+                <button>search</button>
                 <li><a href="#">Kids</a></li>
                 <li><a href="#">Notifications</a></li>
                 <li><a href="#">Profile</a></li>
