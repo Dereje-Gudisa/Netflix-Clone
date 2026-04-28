d8b88aa29b7166107225a821d8de285d65da9e47
https://github.com/Dereje-Gudisa <derejegudisaeme@gmail.com>
2026-04-28 23:55:21 +0300

    account setting page

--- src/Account.jsx
+++ src/Account.jsx
@@ -4,9 +4,15 @@ const Account = () => {
 
   return (
     <>
-      <h1 className='text-white'>Account</h1>
-      <h1 className='text-white'>Account</h1>
-      <h1 className='text-white'>Account</h1>
+      <div className='w-full h-screen bg-gray-500 flex items-center justify-center'>
+        <div className='w-full h-auto text-black text-3xl font-bold border-2 border-gray-500 bg-white flex items-center justify-center'>
+          <div>
+            
+          </div>
+
+          <h1>Account</h1>
+        </div>
+      </div>
     </>
   )
 }
